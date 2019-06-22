91776a2ac97b7ce648e8da9300f2ab1431c29c08
yogaadidr <yogaadidr@users.noreply.github.com>
2019-06-22 21:47:41 +0700

    setting master bancheese

--- src/settings.php
+++ src/settings.php
@@ -17,9 +17,9 @@ return [
         ],
         'db' => [
             'host' => 'localhost',
-            'user' => 'root',
-            'pass' => '',
-            'dbname' => 'db_marketv1',
+            'user' => 'banchees_market',
+            'pass' => 'Bancheese2019',
+            'dbname' => 'banchees_db_marketv1',
             'driver' => 'mysql'
         ]
     ],
